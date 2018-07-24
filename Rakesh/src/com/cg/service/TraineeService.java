917b3739d640ac9d656a7931e95be3116d280040
urstrulyrakesh <40891161+urstrulyrakesh@users.noreply.github.com>
2018-07-24 20:30:18 +0530

    Add files via upload

---
+++ Rakesh/src/com/cg/service/TraineeService.java
@@ -0,0 +1,15 @@
+package com.cg.service;
+
+import java.util.List;
+
+import com.cg.beans.Trainee;
+
+public interface TraineeService {
+
+	public Trainee addDetails(Trainee trainee);
+	public Trainee getDetails(int traineeId);
+	public List<Trainee> getAllDetails();
+	public Trainee delete(int traineeId);
+	public Trainee modify(Trainee trainee);
+	public boolean validate(String a, String b);
+}
